a8f3ec0a2a9d5d06c497975e8796b4a576a28677
b3yb45 <beybaset@gmail.com>
2023-03-08 18:34:03 +0700

    Initial project version

---
+++ main.py
@@ -0,0 +1,94 @@
+from turtle import *
+from math import *
+
+
+def triangle(x, y, turn, side1, side2, angle, border, brdclr, bgclr):
+    """"
+
+    x, y - position
+    turn - starting side1 angle
+    border - border width
+
+    """
+
+    pass
+
+
+def ellipse(x, y, turn, a, b, border, brdclr, bgclr):
+    """"
+
+    x, y - position
+    a, b - semi-axes
+    turn - starting a angle
+    border - border width
+
+    """
+
+    pass
+
+
+def half_ellipse(x, y, turn, a, b, border, brdclr, bgclr):
+    """"
+
+    x, y - position
+    a, b - semi-axes
+    turn - starting a angle
+    border - border width
+
+    """
+
+    pass
+
+
+def rectangle(x, y, turn, a, b, border, brdclr, bgclr):
+    """"
+
+    x, y - position
+    turn - starting a angle
+    border - border width
+
+    """
+
+    pass
+
+
+def n_shape(x, y, turn, length, n, border, brdclr, bgclr):
+    """
+
+    x, y - position
+    turn - starting a angle
+    border - border width
+
+    """
+
+    pass
+
+
+def rhomb(x, y, turn, length, angle, border, brdclr, bgclr):
+    """
+
+    x, y - position
+    turn - starting a angle
+    border - border width
+
+    """
+
+    pass
+
+
+def trap(x, y, turn, a, b, h, angle, border, brdclr, bgclr):
+    """
+
+    x, y - position
+    a, b - bases
+    h - height
+    turn - starting a angle
+    border - border width
+
+    """
+
+    pass
+
+
+if __name__ == '__main__':
+    pass
