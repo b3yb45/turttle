c15ec53ab4699120e35d208c40f5b5c415496220
b3yb45 <beybaset@gmail.com>
2023-03-13 22:41:35 +0700

    drawing

--- main.py
+++ main.py
@@ -278,9 +278,73 @@ def right():
 
 
 def execute_main():
-    left()
-    middle()
-    right()
+    speed(10)
+
+    bgcolor("dodgerblue")
+
+    # Grass.
+    arc(0, -350, 0, 1280, 450, 180, 3, "lawngreen", 'lawngreen')
+
+    # Tree: rec, many ovals.
+    rectangle(630, -85, 90, 500, 105, 1, "sienna", 'sienna')
+
+    arc(590, -70, 0, 150, 100, 360, 1, "forestgreen", 'forestgreen')
+    arc(570, -110, 0, 150, 100, 360, 1, "forestgreen", 'forestgreen')
+    arc(600, -100, 0, 150, 100, 360, 1, "forestgreen", 'forestgreen')
+    arc(500, -75, 0, 150, 100, 360, 1, "forestgreen", 'forestgreen')
+    arc(500, -40, 0, 150, 100, 360, 1, "forestgreen", 'forestgreen')
+    arc(520, -10, 0, 150, 100, 360, 1, "forestgreen", 'forestgreen')
+    arc(580, 10, 0, 150, 100, 360, 1, "forestgreen", 'forestgreen')
+
+    # Sun.
+    arc(375, 225, 0, 125, 125, 360, 1, "gold", 'gold')
+
+    # Clouds.
+    arc(-450, 50, 0, 100, 75, 180, 1, "white", 'white')
+    arc(-380, 50, 0, 100, 75, 180, 1, "white", 'white')
+    arc(-415, 75, 0, 100, 75, 180, 1, "white", 'white')
+
+    arc(-150, 0, 0, 100, 75, 180, 1, "white", 'white')
+    arc(-80, 0, 0, 100, 75, 180, 1, "white", 'white')
+    arc(-115, 25, 0, 100, 75, 180, 1, "white", 'white')
+
+    arc(-220, 120, 0, 100, 75, 180, 1, "white", 'white')
+    arc(-150, 120, 0, 100, 75, 180, 1, "white", 'white')
+    arc(-185, 145, 0, 100, 75, 180, 1, "white", 'white')
+
+    # Duck.
+    arc(-50, -55, 0, 50, 50, 360, 1, "yellow", 'yellow')
+    arc(-50, -55, 0, 10, 10, 360, 1, "black", 'black')
+    triangle(-99, -55, -17, 25, 25, 35, 1, "red", 'red')
+
+    triangle(0, -100, 250, 30, 30, 30, 1, "orange", 'orange')
+    triangle(20, -100, 270, 30, 30, 30, 1, "orange", 'orange')
+
+    arc(10, -80, 0, 100, 50, 360, 1, "yellow", 'yellow')
+    arc(-10, 75, 180, 65, 45, 180, 1, "black", 'gold')
+
+    # Duckling
+    arc(110, -90, 0, 25, 25, 360, 1, "yellow", 'yellow')
+    arc(110, -90, 0, 5, 5, 360, 1, "black", 'black')
+    triangle(87, -90, -15, 10, 10, 30, 1, "red", 'red')
+
+    triangle(135, -115, 250, 15, 15, 30, 1, "orange", 'orange')
+    triangle(145, -115, 270, 15, 15, 30, 1, "orange", 'orange')
+
+    arc(140, -105, 0, 50, 25, 360, 1, "yellow", 'yellow')
+    arc(-140, 103, 180, 32, 22, 180, 1, "black", 'gold')
+
+    # Caterpillar.
+    arc(-400, -240, 0, 30, 30, 360, 1, "hotpink", 'hotpink')
+    arc(-405, -237, 0, 5, 5, 360, 1, "black", 'black')
+    arc(-395, -237, 0, 5, 5, 360, 1, "black", 'black')
+    arc(-390, -220, 0, 10, 10, 360, 1, "deeppink", 'deeppink')
+    arc(-410, -220, 0, 10, 10, 360, 1, "deeppink", 'deeppink')
+
+    arc(-375, -255, 0, 25, 25, 360, 1, "orchid", 'orchid')
+    arc(-350, -245, 0, 22, 22, 360, 1, "purple", 'purple')
+    arc(-332, -260, 0, 19, 19, 360, 1, "royalblue", 'royalblue')
+    arc(-312, -265, 0, 16, 16, 360, 1, "teal", 'teal')
     done()
 
 
